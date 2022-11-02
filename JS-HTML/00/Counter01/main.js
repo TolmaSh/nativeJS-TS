73d0cf4d614dba6379c4766a4fb2af4b0a65c729
TolmaSh <m0v4anua@gmail.com>
2022-11-02 22:40:21 +0200

    feat: counter Js

---
+++ JS-HTML/00/Counter01/main.js
@@ -0,0 +1,24 @@
+
+const refs = {
+    counter: document.querySelector (`#counter`),
+    valueEl: document.querySelector (`#value`),
+    incrementBtn: document.querySelector(`[data-action="increment"]`),
+    decrementBtn: document.querySelector(`[data-action="decrement"]`),
+};
+const counter = {
+    value: 0,
+    increment() {
+        this.value += 1;
+    },
+    decrement() {
+        this.value -= 1;
+    },
+};
+refs.incrementBtn.addEventListener(`click`, function () {
+    counter.increment ();
+    refs.valueEl.textContent = counter.value;
+});
+refs.decrementBtn.addEventListener(`click`, function () {
+    counter.decrement ();
+    refs.valueEl.textContent = counter.value;
+});
