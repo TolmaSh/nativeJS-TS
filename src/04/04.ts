8999b259c7481b507638644c1707decdea556eb8
TolmaSh <m0v4anua@gmail.com>
2023-01-16 19:46:13 +0200

    feat: JS/TS advanced

--- src/04/04.ts
+++
@@ -1,9 +0,0 @@
-import {cityType, governmentBuildingsType} from './04-04';
-
-export const demolishHousesOnTheStreet =  (array: cityType, street: string) => {
-    array.houses = array.houses.filter(house => house.address.street.title !== street)
-}
-
-export const getBuildingsWithStaffCountGreaterThen = (buildingArr: governmentBuildingsType[], staffCount: number) => {
-      return  buildingArr.filter(build => build.staffCount > 500)
-}
